033f1e2c53470851f69df20f5b4ebea0637ef0fe
JannaDev-py <orlandojannael@gmail.com>
2025-06-26 07:44:00 -0600

    gameboard array

--- script.ts
+++ script.ts
@@ -1,17 +1,15 @@
 const canvasContainer = document.querySelector('#gameboard-container') as HTMLDivElement
 const canvas = document.querySelector('#gameboard') as HTMLCanvasElement
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
-
+console.log(ctx)
 canvas.width = canvasContainer.clientWidth
 canvas.height = canvasContainer.clientHeight
 
 const config = {
   wGameboard: canvasContainer.clientWidth,
   hGameboard: canvasContainer.clientHeight,
-  pieceWidth: 15,
-  pieceHeight: 4,
-  piecesRowNum: canvasContainer.clientWidth / 15,
-  piecesColumnNum: canvasContainer.clientHeight / 4
+  pieceWidth: (canvasContainer.clientWidth / 9) - 5,
+  pieceHeight: ((canvasContainer.clientHeight / 2) / 5) - 5
 }
 
 document.addEventListener('resize', () => {
@@ -20,3 +18,24 @@ document.addEventListener('resize', () => {
   config.wGameboard = canvasContainer.clientWidth
   config.hGameboard = canvasContainer.clientHeight
 })
+
+const Pieces = {
+  piecesColumnNum: Math.floor(canvas.width / config.pieceWidth),
+  piecesRowNum: Math.floor(canvas.height / config.pieceHeight / 2)
+}
+
+interface Piece {
+  x: number
+  y: number
+}
+
+const piecesArray: Piece[][] = []
+
+for (let row = 0; row < Pieces.piecesRowNum; row++) {
+  piecesArray.push([])
+  for (let column = 0; column < Pieces.piecesColumnNum; column++) {
+    piecesArray[row].push({ x: column, y: row })
+  }
+}
+
+console.log(piecesArray)
